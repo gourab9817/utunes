0df00b527e31b6d06dc773d72ef58aad2d09f3d3
Gourab Choudhury <c.gourab180@gmail.com>
2025-03-30 02:45:18 +0530

    project start

---
+++ android/app/src/main/kotlin/com/example/utune/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.utune
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:utune/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  // Light Theme
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+
+
+  // Dark Theme
+  static final darkTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+}
